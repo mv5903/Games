8e9ae45cbae5abea4e3ee65652bf99bae0ebeea9
Matthew Vandenberg <mv5903@gmail.com>
2020-12-15 18:42:53 -0500

    Changed gitignore to exclude my test class

--- JavaProjects/src/poker/Test.java
+++ JavaProjects/src/poker/Test.java
@@ -5,10 +5,12 @@ import java.io.UnsupportedEncodingException;
 //♠
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test {
 	public static void main(String[] args)  {
-		
+		Integer[] n = {};
+		List<Integer> list = Arrays.asList(n);
 	}
 	
 
