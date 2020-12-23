a0f3274b50aee0db8cea5117096b9e4ef6aa4584
Matthew Vandenberg <mv5903@gmail.com>
2020-12-23 18:18:01 -0500

    I am instead making the rest api with python.

--- JavaProjects/src/poker/NetIndex.java
+++ JavaProjects/src/poker/NetIndex.java
@@ -1,7 +1,32 @@
 package poker;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 public class NetIndex {
+	static String IPAddress;
+	final static int port = 8080;
+	static Scanner kbd = new Scanner(System.in);
+	static PrintStream charStream;
+	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Welcome to Poker - Internet Version. Are you hosting?");
+		if (kbd.next().equalsIgnoreCase("yes")) {
+			System.out.println("Ok. Please follow the instructions on GitHub to learn how to create a server.");
+			System.exit(0);
+		} else {
+			System.out.println("Please enter the IP Address of whom you would like to connect to.");
+			IPAddress = kbd.nextLine();
+			getPersonalInformation();
+		}
+	}
+	
+	public static void getPersonalInformation() {
+		System.out.println("Please enter your name: ");
+		String name = kbd.next();
+		
 	}
 }
